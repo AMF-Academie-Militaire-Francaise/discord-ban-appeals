cf6671a588b093a846d00eb4f300e14086b98f77
AMF-Academie-Militaire-Francaise <69094166+AMF-Academie-Militaire-Francaise@users.noreply.github.com>
2022-02-23 16:36:40 -0500

    Update oauth.js

--- func/oauth.js
+++ func/oauth.js
@@ -1,6 +1,6 @@
 export async function handler(event, context) {
     const redirectUri = new URL("/.netlify/functions/oauth-callback", DEPLOY_PRIME_URL);
-    let url = `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(process.env.DISCORD_CLIENT_ID)}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify&prompt=none`;
+    let url = `https://discord.com/api/oauth2/authorize?client_id=946152876110127114&redirect_uri=https%3A%2F%2Froblox-canada.netlify.app%2F.netlify%2Ffunctions%2Foauth-callback&response_type=code&scope=identify`;
 
     if (event.queryStringParameters.state !== undefined) {
         url += `&state=${encodeURIComponent(event.queryStringParameters.state)}`;
